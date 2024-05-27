cefad4d78c0f95cf976b8d6f47fd94743f4eb7a4
Seth Miller <millerado@gmail.com>
2024-05-27 17:10:46 -0600

    create get function

--- src/handlers.ts
+++ src/handlers.ts
@@ -3,6 +3,7 @@ import AWS from "aws-sdk";
 import { v4 } from "uuid";
 
 const docClient = new AWS.DynamoDB.DocumentClient();
+const tableName = "ProductsTable";
 
 export const createProduct = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const reqBody = JSON.parse(event.body as string);
@@ -14,7 +15,7 @@ export const createProduct = async (event: APIGatewayProxyEvent): Promise<APIGat
 
   await docClient
     .put({
-      TableName: "ProductsTable",
+      TableName: tableName,
       Item: {
         ...reqBody,
         productID: v4(),
@@ -25,5 +26,30 @@ export const createProduct = async (event: APIGatewayProxyEvent): Promise<APIGat
   return {
     statusCode: 200,
     body: JSON.stringify(product),
+  };
+};
+
+export const getProducts = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const id = event.pathParameters?.id;
+
+  const output = await docClient
+    .get({
+      TableName: tableName,
+      Key: {
+        productID: id,
+      },
+    })
+    .promise();
+
+  if (!output.Item) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ error: "Product not found" }),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(output.Item),
   };
 };
