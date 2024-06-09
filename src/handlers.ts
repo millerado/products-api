b0677737e5a2a030a193828143d91b8a34eb81fe
Seth Miller <millerado@gmail.com>
2024-06-09 10:49:56 -0600

    add update delete and list functions tested

--- src/handlers.ts
+++ src/handlers.ts
@@ -4,6 +4,9 @@ import { v4 } from "uuid";
 
 const docClient = new AWS.DynamoDB.DocumentClient();
 const tableName = "ProductsTable";
+const headers = {
+  "content-type": "application/json",
+};
 
 export const createProduct = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log("event", event);
@@ -17,22 +20,24 @@ export const createProduct = async (event: APIGatewayProxyEvent): Promise<APIGat
   await docClient
     .put({
       TableName: tableName,
-      Item: {
-        ...reqBody,
-        productId: v4(),
-      },
+      Item: product,
     })
     .promise();
 
   return {
     statusCode: 200,
+    headers,
     body: JSON.stringify(product),
   };
 };
 
-export const getProduct = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const id = event.pathParameters?.id;
+class HttpError extends Error {
+  constructor(public statusCody: number, body: Record<string, unknown> = {}) {
+    super(JSON.stringify(body));
+  }
+}
 
+const fetchProductById = async (id: string) => {
   const output = await docClient
     .get({
       TableName: tableName,
@@ -43,14 +48,102 @@ export const getProduct = async (event: APIGatewayProxyEvent): Promise<APIGatewa
     .promise();
 
   if (!output.Item) {
+    throw new HttpError(404, { error: "Product not found" });
+  }
+
+  return output.Item;
+};
+
+const handleError = (error: unknown) => {
+  if (error instanceof HttpError) {
     return {
-      statusCode: 404,
-      body: JSON.stringify({ error: "Product not found" }),
+      statusCode: error.statusCody,
+      headers,
+      body: error.message,
     };
   }
 
+  throw error;
+};
+
+export const getProduct = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  try {
+    const product = await fetchProductById(event?.pathParameters?.id as string);
+
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify(product),
+    };
+  } catch (error) {
+    return handleError(error);
+  }
+};
+
+export const updateProduct = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  try {
+    const id = event?.pathParameters?.id as string;
+
+    await fetchProductById(id);
+
+    const reqBody = JSON.parse(event?.body as string);
+
+    const product = {
+      ...reqBody,
+      productId: id,
+    };
+
+    await docClient
+      .put({
+        TableName: tableName,
+        Item: product,
+      })
+      .promise();
+
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify(product),
+    };
+  } catch (error) {
+    return handleError(error);
+  }
+};
+
+export const deleteProduct = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  try {
+    const id = event?.pathParameters?.id as string;
+
+    await fetchProductById(id);
+
+    await docClient
+      .delete({
+        TableName: tableName,
+        Key: {
+          productId: id,
+        },
+      })
+      .promise();
+
+    return {
+      statusCode: 204,
+      body: "",
+    };
+  } catch (error) {
+    return handleError(error);
+  }
+};
+
+export const listProduct = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const output = await docClient
+    .scan({
+      TableName: tableName,
+    })
+    .promise();
+
   return {
     statusCode: 200,
-    body: JSON.stringify(output.Item),
+    headers,
+    body: JSON.stringify(output.Items),
   };
 };
